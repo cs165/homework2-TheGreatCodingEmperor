a1980d0bdf5451a5009a95a66d72682c4d563a65
TheGreatCodingEmperor <thehandsomeboyno.1@gmail.com>
2019-04-19 13:54:53 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // TODO(you): Write the JavaScript necessary to complete the homework.
 // You can access the RESULTS_MAP from "constants.js" in this file since
 // "constants.js" has been included before "script.js" in index.html.
-const blep = document.querySelector('div[data-choice-id="blep"] .checkbox');
-const btn = document.querySelector('#test');
 function onClick(){
-  blep.style.backgroundColor="black";
+  course.textContent = "successed";
 }
-blep.addEventListener("click",onClick());
+const course = document.querySelector('body article header h1');
+course.addEventListener('click',onClick());
